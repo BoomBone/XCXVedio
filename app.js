15ed67698063b0692709b33904f07519d1136900
Ting <ticccccc@163.com>
2018-12-21 16:36:49 +0800

    初步完成注册

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
 App({
   serverUrl:"http://192.168.0.101:8081",
   userInfo:null,
+
+  setGlobalUserInfo:function(user){
+    wx.setStorageSync("userInfo", user);
+  },
+
+  getGlobalUserInfo:function(){
+    return wx.getStorageSync("userInfo");
+  },
  
 })
--- pages/register/register.js
+++ pages/register/register.js
@@ -1,4 +1,6 @@
 // pages/register/register.js
+const app = getApp()
+
 Page({
 
   /**
@@ -8,59 +10,118 @@ Page({
 
   },
 
+  doRegist: function(e) {
+    var formObject = e.detail.value;
+    var username = formObject.username;
+    var password = formObject.password;
+
+    // 简单验证
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    } else {
+      var serverUrl = app.serverUrl;
+      wx.showLoading({
+        title: '请等待...',
+      });
+      wx.request({
+        url: serverUrl + '/register',
+        method: "POST",
+        data: {
+          username: username,
+          password: password
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function(res) {
+          console.log(res.data);
+          wx.hideLoading();
+          var status = res.data.status;
+          if (status == 200) {
+            wx.showToast({
+                title: "用户注册成功~！！！",
+                icon: 'none',
+                duration: 3000
+              }),
+              // app.userInfo = res.data.data;
+              // fixme 修改原有的全局对象为本地缓存
+              app.setGlobalUserInfo(res.data.data);
+
+          } else if (status == 500) {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        }
+      })
+    }
+  },
+
+  goLoginPage: function() {
+    wx.navigateTo({
+      url: '../login/login',
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
